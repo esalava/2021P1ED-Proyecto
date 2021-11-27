f359f2b040eaf8832636cef02c6a7c4831ebacb4
Andrea Velez <86313886+anvefran@users.noreply.github.com>
2021-11-27 00:13:15 -0500

    Metodo para cargar palabras usando TDA
    Reemplazar la ruta del archivo antes de ejecutar el programa, como lo hice en replit, solo necesitaba el nombre. Se agrega este método para que se use el TDA en lugar de arrays. Por otro lado, falta agregar palabras en vertical y en diagonal.

--- SopaDeLetras/src/main/java/Matrix/Matrix.java
+++ SopaDeLetras/src/main/java/Matrix/Matrix.java
@@ -6,21 +6,25 @@ import TDA.DoblyCircularList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Stack;
+import java.util.Random;
+import java.lang.Character;
 
 public class Matrix {
     private int row;
     private int column;
     
-    private DoblyCircularList<DoblyCircularList<String>> matrixRow;
+    private DoblyCircularList<DoblyCircularList<Character>> matrixRow;
     
     public Matrix(int row, int column ){
         this.row = row;
         this.column = column;
         matrixRow = new DoblyCircularList<>();
         for(int i = 0; i < row ; i++){
-            DoblyCircularList<String> actualrow = new DoblyCircularList<>();
+            DoblyCircularList<Character> actualrow = new DoblyCircularList<>();
             for(int j = 0; j<column; j++){
-                actualrow.addLast("0"); // valor con el que se inicializa la matriz
+              Random random = new Random();
+              char rand = (char) (random.nextInt(26) + 'A');   //Letra aleatoria
+                actualrow.addLast(rand); // valor con el que se inicializa la matriz
             }
             matrixRow.addLast(actualrow);
         }
@@ -29,10 +33,46 @@ public class Matrix {
     //metodo para mostrar la matriz y sus valores
     public void showMatrix(){
         for(int i = 0; i < row ; i++){
-            DoblyCircularList<String> actualrow = matrixRow.getIndex(i);
+            DoblyCircularList<Character> actualrow = matrixRow.getIndex(i);
             actualrow.show();
         }
     }
+    
+    //ACTUALIZAR RUTA, YO LO HICE EN REPLIT POR ESO NO NECESITABA RUTA, si funciona
+    //metodo para cargar palabras a la sopa de letras
+    
+    public void agregarPalabras(){
+      DoblyCircularList<String> listaPalabras = leerPalabras("palabras.txt");
+      Random r = new Random();
+      int x = r.nextInt(row);
+      int y = r.nextInt(column);
+
+      int indexpalabra = r.nextInt(listaPalabras.size());
+      String palabra = listaPalabras.getIndex(indexpalabra);
+
+      while (palabra.length()>column){
+          indexpalabra = r.nextInt(listaPalabras.size());
+          palabra = listaPalabras.getIndex(indexpalabra);
+      }
+      
+        //horizontal
+
+        DoblyCircularList<Character> actualRow = matrixRow.getIndex(x);
+        
+        for (int i=0;i<palabra.length();i++){
+          Character letra = Character.toUpperCase(palabra.charAt(i));
+          int indice = i+y;
+          if (indice<column){
+            boolean conf = actualRow.setAt(letra,indice);
+          } else {
+            indice = indice-column;
+            boolean conf = actualRow.setAt(letra,indice);
+          }
+        }
+      
+    }
+      
+    
     
     //METODO PARA LEER LAS PALABRAS DEL ARCHIVO, RETORNA LISTA
     public static DoblyCircularList<String> leerPalabras(String ruta){
@@ -51,35 +91,6 @@ public class Matrix {
         return listaPalabras;
     }
     
-    //METODO PARA AGREGAR PALABRA EN DIAGONAL
-    public static void agregarDiagonal(String palabra, Cell sopa[][], int fila, int columna){
-        
-        for (int i=0; i<palabra.length();i++){
-            Cell celda = sopa[fila+i][columna + i];
-            celda.setLetter(String.valueOf(palabra.charAt(i)));
-            sopa[fila+i][columna +i] = celda;
-        }
-    
-    }
-
-    //METODO PARA AGREGAR PALABRA EN HORIZONTAL
-    public static void agregarHorizontal(String palabra, Cell sopa[][], int fila, int columna){
-    
-        for (int i=0; i<palabra.length();i++){
-            Cell celda = sopa[fila][columna + i];
-            celda.setLetter(String.valueOf(palabra.charAt(i)));
-            sopa[fila][columna + i] = celda;
-        }
-    }
-
-    //METODO PARA AGREGAR PALABRA EN VERTICAL
-    public static void agregarVertical(String palabra, Cell sopa[][], int filas, int columnas){
-        for (int i=0; i<palabra.length();i++){
-            Cell celda = sopa[filas+i][columnas];
-            celda.setLetter(String.valueOf(palabra.charAt(i)));
-            sopa[filas+i][columnas] = celda;
-        }
-    }
     
     public static String invertirPalabra(String palabra){
         Stack<String> pila = new Stack<>();
